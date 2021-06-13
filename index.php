70715e8aede04eab8d912aa44f3b75992ecf63f4
Louiza-Dev <louizaachiche@gmail.com>
2021-06-13 21:21:53 +0200

    Depot de 2emExoPart1 PHP

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="main.css">
+    <title>Part1 - Exo3</title>
+  </head>
+  <body>
+    <h1>Part1 - Exo3</h1>
+    <?php
+    $age = 16;
+    echo $age;
+    if ($age <= 18) {
+        echo ' ans, tu es mineur';
+    } else {
+        echo ' ans,tu es majeur';
+    }
+    ?>
+  </body>
+</html>
